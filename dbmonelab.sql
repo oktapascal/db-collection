46079c58ab0ca9dbfae3edacb46880f74f812ba0
oktapascal <oktaiscool@gmail.com>
2020-11-02 14:46:21 +0700

    first commit

---
+++ dbmonelab.sql
@@ -0,0 +1,368 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 02 Nov 2020 pada 08.45
+-- Versi server: 10.4.13-MariaDB
+-- Versi PHP: 7.2.32
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `dbmonelab`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `bahan`
+--
+
+CREATE TABLE `bahan` (
+  `id_bahan` varchar(10) NOT NULL,
+  `nama_bahan` varchar(100) NOT NULL,
+  `stok` float NOT NULL,
+  `satuan` varchar(10) NOT NULL,
+  `jenis` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `bom`
+--
+
+CREATE TABLE `bom` (
+  `id_kopi` varchar(10) NOT NULL,
+  `id_bahan` varchar(10) NOT NULL,
+  `jumlah` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `coa`
+--
+
+CREATE TABLE `coa` (
+  `no_coa` varchar(50) NOT NULL,
+  `nama_coa` varchar(100) NOT NULL,
+  `header_coa` varchar(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `detail_pembelian`
+--
+
+CREATE TABLE `detail_pembelian` (
+  `id_pembelian` varchar(10) NOT NULL,
+  `id_bahan` varchar(10) NOT NULL,
+  `jumlah` float NOT NULL,
+  `harga` int(11) NOT NULL,
+  `nominal` int(11) NOT NULL,
+  `id_pesanan` varchar(10) NOT NULL,
+  `id_kopi` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `detail_pesanan`
+--
+
+CREATE TABLE `detail_pesanan` (
+  `id_pesanan` varchar(10) NOT NULL,
+  `id_kopi` varchar(10) NOT NULL,
+  `id_reseller` varchar(10) NOT NULL,
+  `jumlah` int(11) NOT NULL,
+  `nominal` int(11) NOT NULL,
+  `status` varchar(2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `jurnal`
+--
+
+CREATE TABLE `jurnal` (
+  `id_transaksi` varchar(10) NOT NULL,
+  `no_coa` varchar(50) NOT NULL,
+  `posisi` varchar(1) NOT NULL,
+  `tanggal` date NOT NULL,
+  `nominal` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `kopi`
+--
+
+CREATE TABLE `kopi` (
+  `id_kopi` varchar(10) NOT NULL,
+  `nama_kopi` varchar(100) NOT NULL,
+  `jenis_kopi` varchar(50) NOT NULL,
+  `harga` int(11) NOT NULL,
+  `berat` int(11) NOT NULL,
+  `btkl` int(11) NOT NULL,
+  `listrik` int(11) NOT NULL,
+  `mesin` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pembelian`
+--
+
+CREATE TABLE `pembelian` (
+  `id_pembelian` varchar(10) NOT NULL,
+  `tanggal` date NOT NULL,
+  `periode` varchar(6) NOT NULL,
+  `nominal` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pesanan`
+--
+
+CREATE TABLE `pesanan` (
+  `id_pesanan` varchar(10) NOT NULL,
+  `tanggal` date NOT NULL,
+  `periode` varchar(6) NOT NULL,
+  `nominal` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `produksi`
+--
+
+CREATE TABLE `produksi` (
+  `id_produksi` varchar(10) NOT NULL,
+  `periode` varchar(6) NOT NULL,
+  `tanggal` date NOT NULL,
+  `total` int(11) NOT NULL,
+  `bbb` int(11) NOT NULL,
+  `btkl` int(11) NOT NULL,
+  `bop` int(11) NOT NULL,
+  `id_pesanan` varchar(10) NOT NULL,
+  `id_kopi` varchar(10) NOT NULL,
+  `taksiran_jumlah` int(11) NOT NULL,
+  `taksiran_bop` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `reseller`
+--
+
+CREATE TABLE `reseller` (
+  `id_reseller` varchar(10) NOT NULL,
+  `nama_reseller` varchar(50) NOT NULL,
+  `alamat` varchar(100) NOT NULL,
+  `no_telp` varchar(13) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `saldo`
+--
+
+CREATE TABLE `saldo` (
+  `id_saldo` varchar(10) NOT NULL,
+  `periode` varchar(6) NOT NULL,
+  `nominal` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `transaksi`
+--
+
+CREATE TABLE `transaksi` (
+  `id_transaksi` varchar(10) NOT NULL,
+  `periode` varchar(6) NOT NULL,
+  `nominal` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `user`
+--
+
+CREATE TABLE `user` (
+  `id_user` varchar(20) NOT NULL,
+  `username` varchar(10) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `jabatan` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `bahan`
+--
+ALTER TABLE `bahan`
+  ADD PRIMARY KEY (`id_bahan`);
+
+--
+-- Indeks untuk tabel `coa`
+--
+ALTER TABLE `coa`
+  ADD PRIMARY KEY (`no_coa`);
+
+--
+-- Indeks untuk tabel `detail_pembelian`
+--
+ALTER TABLE `detail_pembelian`
+  ADD KEY `fk_id_pembelian_pembelian` (`id_pembelian`),
+  ADD KEY `fk_id_bahan_pembelian` (`id_bahan`),
+  ADD KEY `fk_id_pesanan_pembelian` (`id_pesanan`),
+  ADD KEY `fk_id_kopi_pembelian` (`id_kopi`);
+
+--
+-- Indeks untuk tabel `detail_pesanan`
+--
+ALTER TABLE `detail_pesanan`
+  ADD KEY `fk_id_kopi_pesanan` (`id_kopi`),
+  ADD KEY `fk_id_pesanan_pesanan` (`id_pesanan`),
+  ADD KEY `fk_id_reseller_pesanan` (`id_reseller`);
+
+--
+-- Indeks untuk tabel `jurnal`
+--
+ALTER TABLE `jurnal`
+  ADD KEY `fk_id_transaksi_jurnal` (`id_transaksi`),
+  ADD KEY `fk_no_coa` (`no_coa`);
+
+--
+-- Indeks untuk tabel `kopi`
+--
+ALTER TABLE `kopi`
+  ADD PRIMARY KEY (`id_kopi`);
+
+--
+-- Indeks untuk tabel `pembelian`
+--
+ALTER TABLE `pembelian`
+  ADD PRIMARY KEY (`id_pembelian`);
+
+--
+-- Indeks untuk tabel `pesanan`
+--
+ALTER TABLE `pesanan`
+  ADD PRIMARY KEY (`id_pesanan`);
+
+--
+-- Indeks untuk tabel `produksi`
+--
+ALTER TABLE `produksi`
+  ADD PRIMARY KEY (`id_produksi`),
+  ADD KEY `fk_id_pesanan` (`id_pesanan`),
+  ADD KEY `fk_id_kopi_produksi` (`id_kopi`);
+
+--
+-- Indeks untuk tabel `reseller`
+--
+ALTER TABLE `reseller`
+  ADD PRIMARY KEY (`id_reseller`);
+
+--
+-- Indeks untuk tabel `saldo`
+--
+ALTER TABLE `saldo`
+  ADD PRIMARY KEY (`id_saldo`);
+
+--
+-- Indeks untuk tabel `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD PRIMARY KEY (`id_transaksi`);
+
+--
+-- Indeks untuk tabel `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `bom`
+--
+ALTER TABLE `bom`
+  ADD CONSTRAINT `fk_id_bahan` FOREIGN KEY (`id_bahan`) REFERENCES `bahan` (`id_bahan`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_id_kopi` FOREIGN KEY (`id_kopi`) REFERENCES `kopi` (`id_kopi`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `detail_pembelian`
+--
+ALTER TABLE `detail_pembelian`
+  ADD CONSTRAINT `fk_id_bahan_pembelian` FOREIGN KEY (`id_bahan`) REFERENCES `bahan` (`id_bahan`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_id_kopi_pembelian` FOREIGN KEY (`id_kopi`) REFERENCES `kopi` (`id_kopi`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_id_pembelian_pembelian` FOREIGN KEY (`id_pembelian`) REFERENCES `pembelian` (`id_pembelian`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_id_pesanan_pembelian` FOREIGN KEY (`id_pesanan`) REFERENCES `pesanan` (`id_pesanan`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `detail_pesanan`
+--
+ALTER TABLE `detail_pesanan`
+  ADD CONSTRAINT `fk_id_kopi_pesanan` FOREIGN KEY (`id_kopi`) REFERENCES `kopi` (`id_kopi`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_id_pesanan_pesanan` FOREIGN KEY (`id_pesanan`) REFERENCES `pesanan` (`id_pesanan`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_id_reseller_pesanan` FOREIGN KEY (`id_reseller`) REFERENCES `reseller` (`id_reseller`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `jurnal`
+--
+ALTER TABLE `jurnal`
+  ADD CONSTRAINT `fk_id_transaksi_jurnal` FOREIGN KEY (`id_transaksi`) REFERENCES `transaksi` (`id_transaksi`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_no_coa` FOREIGN KEY (`no_coa`) REFERENCES `coa` (`no_coa`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `pembelian`
+--
+ALTER TABLE `pembelian`
+  ADD CONSTRAINT `fk_id_pembelian` FOREIGN KEY (`id_pembelian`) REFERENCES `transaksi` (`id_transaksi`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `produksi`
+--
+ALTER TABLE `produksi`
+  ADD CONSTRAINT `fk_id_kopi_produksi` FOREIGN KEY (`id_kopi`) REFERENCES `kopi` (`id_kopi`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_id_pesanan` FOREIGN KEY (`id_pesanan`) REFERENCES `pesanan` (`id_pesanan`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_id_produksi` FOREIGN KEY (`id_produksi`) REFERENCES `transaksi` (`id_transaksi`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `saldo`
+--
+ALTER TABLE `saldo`
+  ADD CONSTRAINT `fk_id_transaksi` FOREIGN KEY (`id_saldo`) REFERENCES `transaksi` (`id_transaksi`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
