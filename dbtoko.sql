36e15275605679f57e9caf98b3efc08ecbdc6280
Okta Pascal Ibrahim <oktaiscool@gmail.com>
2020-11-24 17:12:43 +0700

    Tambah data produk

--- dbtoko.sql
+++ dbtoko.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 22 Nov 2020 pada 15.51
+-- Waktu pembuatan: 24 Nov 2020 pada 11.09
 -- Versi server: 10.4.14-MariaDB
 -- Versi PHP: 7.4.11
 
@@ -89017,6 +89017,21 @@ CREATE TABLE `produk` (
   `foto` varchar(150) NOT NULL,
   `id_subkategori` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `produk`
+--
+
+INSERT INTO `produk` (`id_produk`, `nama_produk`, `diskon_produk`, `satuan_produk`, `foto`, `id_subkategori`) VALUES
+('BRG.202011.00001', 'Nestle Cereal Breakfast Koko Krunch 80g', 0, 'pcs', 'BRG.202011.00001.jpg', 'SKG.202011.00001'),
+('BRG.202011.00002', 'Simba Cereal Choco Chips Coklat 170g', 0, 'pcs', 'BRG.202011.00002.jpg', 'SKG.202011.00001'),
+('BRG.202011.00003', 'Quaker Instant Oatmeal Merah 1.2kg', 0, 'pcs', 'BRG.202011.00003.jpg', 'SKG.202011.00001'),
+('BRG.202011.00004', 'Energen Cereal & Susu Instant Cokelat 10x29g', 0, 'pcs', 'BRG.202011.00004.jpg', 'SKG.202011.00001'),
+('BRG.202011.00005', 'Energen Cereal & Susu Instant Vanilla 10x29g', 0, 'pcs', 'BRG.202011.00005.jpg', 'SKG.202011.00001'),
+('BRG.202011.00006', 'Al-Shifa Natural Honey 250g ', 0, 'toples', 'BRG.202011.00006.jpg', 'SKG.202011.00002'),
+('BRG.202011.00007', 'Nusantara Madu Murni 250ml', 0, 'botol', 'BRG.202011.00007.jpg', 'SKG.202011.00002'),
+('BRG.202011.00008', 'Tresno Joyo Madu Tj Murni 150g', 0, 'toples', 'BRG.202011.00008.jpg', 'SKG.202011.00002'),
+('BRG.202011.00009', 'Airmancur Madurasa Original 150g', 0, 'toples', 'BRG.202011.00009.jpg', 'SKG.202011.00002');
 
 -- --------------------------------------------------------
 
