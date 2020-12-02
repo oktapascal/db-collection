0e2f459d4616b74489912a573545b5ba22e8e578
Okta Pascal Ibrahim <oktaiscool@gmail.com>
2020-12-02 16:42:00 +0700

    Update dbtoko

--- dbtoko.sql
+++ dbtoko.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.3
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 29 Nov 2020 pada 03.12
--- Versi server: 10.4.14-MariaDB
--- Versi PHP: 7.4.11
+-- Waktu pembuatan: 02 Des 2020 pada 10.41
+-- Versi server: 10.4.13-MariaDB
+-- Versi PHP: 7.2.32
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
